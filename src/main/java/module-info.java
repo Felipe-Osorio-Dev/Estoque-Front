ce6ee353d72822351eef47ed5c12ef7593157f2c
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-01-14 04:14:22 -0300

    build: Adicionado modulos do Jackson ao projeto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module com.dev.osorio {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
 
     opens com.dev.osorio to javafx.fxml;
     exports com.dev.osorio;
