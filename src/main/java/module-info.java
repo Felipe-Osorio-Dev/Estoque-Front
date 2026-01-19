33f84c631719f82a3bc65ea18c81ee4f1b2eead0
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-01-19 01:43:25 -0300

    build: Adicionado exports de pacotes ao module-info.java para que o mapper possa ter acesso.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,14 @@ module com.dev.osorio {
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
+    requires java.net.http;
+    requires org.mapstruct;
 
     opens com.dev.osorio to javafx.fxml;
+    opens com.dev.osorio.controller.main to javafx.fxml;
+    opens com.dev.osorio.controller.product to javafx.fxml;
     exports com.dev.osorio;
+    exports  com.dev.osorio.dto;
+    exports  com.dev.osorio.model;
+    exports com.dev.osorio.mapper;
 }
