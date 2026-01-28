d3719bf88e3480a9270a5d6f02dc0370f4591e12
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-01-28 03:58:52 -0300

    refactor: Navegação do Sistema refatorada, removendo o seu estado static e adicionando injeção de dependencias via Consumer.

--- src/main/java/com/dev/osorio/App.java
+++ src/main/java/com/dev/osorio/App.java
@@ -1,9 +1,17 @@
 package com.dev.osorio;
 
+import com.dev.osorio.config.HttpConfig;
+import com.dev.osorio.config.JsonObjectConfig;
+import com.dev.osorio.controller.main.MainController;
+import com.dev.osorio.controller.product.ProductController;
 import com.dev.osorio.navigation.AppNavigation;
+import com.dev.osorio.presenter.MainPresenter;
+import com.dev.osorio.presenter.ProductPresenter;
+import com.dev.osorio.service.ProductService;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -11,15 +19,28 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
+    private final AppNavigation appNavigation = new AppNavigation();
+    private final JsonObjectConfig jsonObjectConfig = new JsonObjectConfig();
+    private final HttpConfig httpConfig = new HttpConfig();
+
     @Override
     public void start(Stage stage) {
 
         //Carregar as Views do App
-        AppNavigation.loadView("mainView", "main/MainView.fxml");
-        AppNavigation.loadView("productView", "product/ProductView.fxml");
+        appNavigation.loadView("mainView", "main/MainView.fxml", (MainController controller) -> {
+            controller.setPresenter(new MainPresenter(appNavigation));
+        });
+        appNavigation.loadView("productView", "product/ProductView.fxml", (ProductController controller) -> {
+            controller.setPresenter(new ProductPresenter(
+                    new ProductService(httpConfig, jsonObjectConfig), appNavigation)
+            );
+        });
 
         //Inicializar a Janela Principal do App
-        Parent root = AppNavigation.initialize("main/MainView.fxml");
+        Parent root = appNavigation.getView("mainView");
+
+        //Seta o root
+        appNavigation.setRoot((BorderPane) root);
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
--- src/main/java/com/dev/osorio/navigation/AppNavigation.java
+++ src/main/java/com/dev/osorio/navigation/AppNavigation.java
@@ -8,41 +8,39 @@ import javafx.scene.layout.BorderPane;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class AppNavigation {
 
-    private static BorderPane root;
-    private static final Map<String, Parent> views =  new HashMap<>();
+    private BorderPane root;
+    private final Map<String, Parent> views =  new HashMap<>();
 
-    public static Parent initialize(String fxmlPath) {
-
-        try {
-
-            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxmlPath));
-            root = fxmlLoader.load();
-
-            return root;
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public void setRoot(BorderPane root) {
+        this.root = root;
     }
 
-    public static void loadView(String fxmlName, String fxmlPath) {
+    public Parent getView(String viewName) {
+        return views.get(viewName);
+    }
 
+    public <T> void loadView(String fxmlName, String fxmlPath, Consumer<T> injector) {
         try {
+            FXMLLoader loader = new FXMLLoader(App.class.getResource(fxmlPath));
+            Parent view = loader.load();
 
-            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxmlPath));
-            Parent view = fxmlLoader.load();
+            T controller = loader.getController();
+
+            if (injector != null) {
+                injector.accept(controller);
+            }
 
             views.put(fxmlName, view);
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public static void navigateTo(String fxmlName) {
-        root.setCenter(views.get(fxmlName));
+    public void navigateTo(String fxmlName) {
+        this.root.setCenter(this.views.get(fxmlName));
     }
 }
