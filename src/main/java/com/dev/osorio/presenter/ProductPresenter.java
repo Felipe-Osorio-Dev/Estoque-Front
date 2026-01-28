755f60eb16bdfa64908bb67a26a605cfdb8efe00
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-01-28 04:05:36 -0300

    refactor: Adicionado novas dependencias, mapper para conversao dos DTOs em MODEL e a dependencia de navegação.

--- src/main/java/com/dev/osorio/presenter/ProductPresenter.java
+++ src/main/java/com/dev/osorio/presenter/ProductPresenter.java
@@ -1,6 +1,9 @@
 package com.dev.osorio.presenter;
 
 import com.dev.osorio.dto.response.ProductResponse;
+import com.dev.osorio.mapper.ProductMapper;
+import com.dev.osorio.model.ProductModel;
+import com.dev.osorio.navigation.AppNavigation;
 import com.dev.osorio.service.ProductService;
 import javafx.concurrent.Task;
 
@@ -9,12 +12,15 @@ import java.util.function.Consumer;
 public class ProductPresenter {
 
     private final ProductService productService;
+    private final ProductMapper productMapper = ProductMapper.INSTANCE;
+    private final AppNavigation  appNavigation;
 
-    public ProductPresenter(ProductService productService) {
+    public ProductPresenter(ProductService productService, AppNavigation appNavigation) {
         this.productService = productService;
+        this.appNavigation = appNavigation;
     }
 
-    public void getProductByData(String data, Consumer<ProductResponse> response, Consumer<Throwable> error) {
+    public void getProductByData(String data, Consumer<ProductModel> response, Consumer<Throwable> error) {
 
         Task<ProductResponse> task = new Task<>() {
             @Override
@@ -23,7 +29,7 @@ public class ProductPresenter {
             }
         };
 
-        task.setOnSucceeded(e -> response.accept(task.getValue()));
+        task.setOnSucceeded(e -> response.accept(productMapper.toModel(task.getValue())));
         task.setOnFailed(e -> error.accept(task.getException().getCause()));
 
         new Thread(task).start();
