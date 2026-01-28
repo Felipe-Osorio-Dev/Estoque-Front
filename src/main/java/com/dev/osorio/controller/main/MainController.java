6772b803a9396227072592dda9612c62d56ba36e
Felipe.Osorio <felipe.osorio.dev@gmail.com>
2026-01-28 04:01:52 -0300

    refactor: Adicionado metodo para injeção de dependencia do presenter.

--- src/main/java/com/dev/osorio/controller/main/MainController.java
+++ src/main/java/com/dev/osorio/controller/main/MainController.java
@@ -1,12 +1,18 @@
 package com.dev.osorio.controller.main;
 
-import com.dev.osorio.navigation.AppNavigation;
+import com.dev.osorio.presenter.MainPresenter;
 import javafx.fxml.FXML;
 
 public class MainController {
 
+    private MainPresenter mainPresenter;
+
+    public void setPresenter(MainPresenter mainPresenter) {
+        this.mainPresenter = mainPresenter;
+    }
+
     @FXML
     public void navigateToProduct() {
-        AppNavigation.navigateTo("productView");
+        mainPresenter.navigateTo("productView");
     }
 }
